3c0ad15d18fa761a9cd9e3040dd4aa1a39c8ddfd
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-11-14 20:32:50 +0000

    Create template for lesson requests

--- msms/urls.py
+++ msms/urls.py
@@ -20,4 +20,5 @@ from lessons import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('lesson-request/', views.lesson_request, name = 'lesson_request')
 ]
