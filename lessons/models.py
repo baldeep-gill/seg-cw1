88777bcee44b6a7023bbae5fbd69a451f3702e90
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-11-14 18:16:39 +0000

    Start implementation of Lesson Request model and tests

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class LessonRequests(models.Model):
+    availability = models.CharField(
+        blank = False,
+        max_length = 280,
+    )
+
+    lessonNum = models.IntegerField(
+        unique = False,
+        blank = False,
+    )
+
+    interval = models.IntegerField(
+        unique = False,
+        blank = False,
+    )
+
+    duration = models.IntegerField(
+        unique = False,
+        blank = False,
+    )
+
+    topic = models.CharField(
+        blank = False,
+        max_length = 50,
+    )
+
+    teacher = models.CharField(
+        blank = True,
+        max_length = 50,
+    )
