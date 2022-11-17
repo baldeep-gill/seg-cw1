0c8e368c0a2620994daaca05f5df83b100e922dd
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-17 18:50:38 +0000

    Allow adding more than one type of user
    - Refactor the student model completely to allow adding a new type of user
    - Add base User model
    - Add Admin model
    - Add StudentProfile model to include the student number
    - Adjust forms.py to account for the changes of models
    - Adjust settings.py to include the new base model
    - Add the Admin model to admins.py so now we can create a new admin
    - Create a superuser with username admin and password admin

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,7 +1,7 @@
 """Forms for the lessons app."""
 from django import forms
 from django.core.validators import RegexValidator
-from .models import Student
+from .models import User, Student, StudentProfile
 from django.db.models import Max
 
 class StudentSignUpForm(forms.ModelForm):
@@ -35,26 +35,22 @@ class StudentSignUpForm(forms.ModelForm):
             self.add_error('password_confirmation', 'Confirmation does not match password.')
 
     def find_id_for_new_student(self):
-        id = 1
-        while Student.objects.filter(student_number=id):
-            id += 1
-        return id
+        return User.objects.last().id + 1 
 
     def save(self):
         """Create a new user."""
         new_student_number = self.find_id_for_new_student()
 
         super().save(commit=False)
-        student = Student.objects.create_user(
-            #username = self.cleaned_data.get('username'),
+        student = Student.objects.create(
             username = self.cleaned_data.get('first_name') + self.cleaned_data.get('last_name') + f'{new_student_number}' ,
             first_name=self.cleaned_data.get('first_name'),
             last_name=self.cleaned_data.get('last_name'),
             email=self.cleaned_data.get('email'),
             password=self.cleaned_data.get('new_password'),
-            #student_number = self.cleaned_data.get('student_number')
-            student_number = new_student_number
         )
+
+        StudentProfile.objects.create(user=student, student_number = new_student_number)
         return student
 
 
