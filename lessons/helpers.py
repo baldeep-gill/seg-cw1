f226b1dab96424b63693d546624d34f5a30ef1af
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-19 14:31:04 +0000

    Improve find_next_available_student_number() in helpers.py

--- lessons/helpers.py
+++ lessons/helpers.py
@@ -2,7 +2,4 @@ from .models import Student
 
 def find_next_available_student_number():
     """Will find the next available student number for a new student"""
-    id = 1
-    while Student.objects.filter(student_number=id):
-        id += 1
-    return id
+    return Student.objects.last().id + 1 
