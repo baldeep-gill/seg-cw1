585f4e1ee2026742c83e321fd9ca8181c49fb7f9
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-19 14:42:18 +0000

    Update seed.py to follow closely what was laid in the videos for testing

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.utils import IntegrityError
 from faker import Faker
 from lessons.models import Student, StudentProfile
-from lessons.management.commands.unseed import Command as Unseeder
 from lessons.helpers import find_next_available_student_number
 
 class Command(BaseCommand):
@@ -14,16 +13,12 @@ class Command(BaseCommand):
         self.faker = Faker('en_GB')
 
     def handle(self, *args, **options):
-        user_count = 0
-        Unseeder.handle(self)
-        self._create_named_student_user(firstname="John", lastname="Doe", email="john.doe@example.org", password="Password123")
-        
+        user_count = 0        
         while user_count < Command.USER_COUNT:
             print(f'Seeding user {user_count}',  end='\r')
             try:
                 self._create_user()
             except (IntegrityError):
-                # print("epic fail")
                 continue
             user_count += 1
         print('User seeding complete')
@@ -32,22 +27,25 @@ class Command(BaseCommand):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
-        self._create_named_student_user(first_name, last_name, email, Command.PASSWORD)
+        username = self._username(first_name, last_name)
+        self._create_named_student_user(first_name, last_name, email, username, Command.PASSWORD)
        
-    
-    def _create_named_student_user(self, firstname, lastname, email, password):
+    '''un stands for username'''
+    def _create_named_student_user(self, firstname, lastname, uname, email, password):
         studentnumber = find_next_available_student_number()
-
-
-        created_student = Student.objects.create_user(
+        created_student = Student.objects.create(
             first_name=firstname,
             last_name=lastname,
             email=email,
+            username=uname,
             password=password,
         )
-        print("Error creating user")
-        # StudentProfile.objects.create(user=created_student, student_number=studentnumber)
+        StudentProfile.objects.create(user=created_student, student_number=studentnumber)
 
     def _email(self, first_name, last_name):
         email = f'{first_name}.{last_name}@example.org'
         return email
+    
+    def _username(self, first_name, last_name):
+        username = f'@{first_name}{last_name}'
+        return username
