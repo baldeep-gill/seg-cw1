75f4c6b65b60081ea8085155f9c52c461249b131
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-19 15:37:57 +0000

    Update find_next_available_student_number() in helpers.py
    - Add an if statement to check if there is a student in the first place. This will help if there are no students created

--- lessons/helpers.py
+++ lessons/helpers.py
@@ -2,4 +2,6 @@ from .models import Student
 
 def find_next_available_student_number():
     """Will find the next available student number for a new student"""
-    return Student.objects.last().id + 1 
+    if(Student.objects.last()):
+        return Student.objects.last().id + 1 
+    return 1
