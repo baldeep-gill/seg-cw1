6c6617a43869b2ac1efd5807d79a02ee57d51da8
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-20 16:50:38 +0000

    Integrate tests with the new model of Student and Admin
    - Change the way tests are set up so to accomodate for the new models
    - Update fixtures (remove student_number field and create a super_user fixture)
    - Update seeding tests to include StudentProfile, Student, Admin, and User
    - All 32 tests pass

--- lessons/tests/seeding/test_students_seeding_unseeding.py
+++ lessons/tests/seeding/test_students_seeding_unseeding.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lessons.models import Student
+from lessons.models import Student, Admin, User, StudentProfile
 from django.core.management import call_command
 
 # Create your tests here.
@@ -10,10 +10,22 @@ class UserModeTestCase(TestCase):
         call_command('seed')
         # the seeder creates 100 student users using Faker
         # as well as 1 user with specified information (John Doe)
-        student_count = Student.objects.count()
-        self.assertEqual(student_count, 101)
+        student_count = Student.students.count()
+        admins_count = Admin.admins.count()
+        user_count = User.objects.count()
+        student_profile_count = StudentProfile.objects.count()
+        self.assertEqual(student_count, 100)
+        self.assertEqual(admins_count, 100)
+        self.assertEqual(student_profile_count, 100)
+        self.assertEqual(user_count, 200)
 
     def test_unseeding_clears_student_users(self):
         call_command('unseed')
-        student_count = Student.objects.count()
+        student_count = Student.students.count()
+        admins_count = Admin.admins.count()
+        student_profile_count = StudentProfile.objects.count()
+        user_count = User.objects.count()
         self.assertEqual(student_count, 0)
+        self.assertEqual(admins_count, 0)
+        self.assertEqual(student_profile_count, 0)
+        self.assertEqual(user_count, 0)
