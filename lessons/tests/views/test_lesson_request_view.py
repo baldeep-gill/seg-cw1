71084be442b7b59e58fef9188737c911ad776f55
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-11-21 18:43:16 +0000

    Start tests for lesson request view
    - Make minor change to lesson request url (/lesson-request/ to
    /lesson_request/)

---
+++ lessons/tests/views/test_lesson_request_view.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+from lessons.forms import LessonRequestForm
+from lessons.models import Student, LessonRequest
+
+class LessonRequestViewTestCase(TestCase):
+    """Tests for the lesson request view"""
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.url = reverse('lesson_request')
+
+    def test_login_url(self):
+        self.assertEqual(self.url, '/lesson_request/')
