68b27f19aa1dc31597465312d83c9ea66e19b4d7
Baldeep <baldeep.gill@kcl.ac.uk>
2022-11-24 16:55:41 +0000

    Add tests for request edit form

--- lessons/tests/forms/test_edit_request_form.py
+++ lessons/tests/forms/test_edit_request_form.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django import forms
-from lessons.models import Student
-from lessons.forms import LessonRequestForm
+from lessons.models import Student, LessonRequest
+from lessons.forms import EditForm
 
 class LessonRequestFromTestCase(TestCase):
-    """Unit tests for lesson request form"""
+    """Unit tests for lesson request edit form"""
 
     fixtures = [
         'lessons/tests/fixtures/default_student.json',
@@ -16,15 +16,32 @@ class LessonRequestFromTestCase(TestCase):
         self.student = Student.objects.get(email="johndoe@example.org")
         self.form_input = {
             "availability": "Monday",
-            "lessonNum": 3,
+            "lessonNum": 2,
             "interval": 1,
             "duration": 60,
             "topic": "piano",
             "teacher": "Mr Bob"
         }
 
+        self.lessonRequest = LessonRequest(
+            author = self.student,
+            availability = "Monday",
+            lessonNum = 2,
+            interval = 1,
+            duration = 60,
+            topic = "Piano",
+            teacher = "bob"
+        )
+
+    def test_request_edit(self):
+        self.form_input['availability'] = "Tuesday"
+        form = EditForm(instance=self.lessonRequest, data=self.form_input)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual("Tuesday", self.lessonRequest.availability)
+
     def test_form_contains_fields(self):
-        form = LessonRequestForm()
+        form = EditForm()
         self.assertIn("availability", form.fields)
         self.assertIn("lessonNum", form.fields)
         self.assertIn("interval", form.fields)
@@ -33,35 +50,35 @@ class LessonRequestFromTestCase(TestCase):
         self.assertIn("teacher", form.fields)
 
     def test_accept_valid_input(self):
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_reject_blank_availability(self):
         self.form_input['availability'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_reject_blank_lesson_num(self):
         self.form_input['lessonNum'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_reject_blank_interval(self):
         self.form_input['interval'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_reject_blank_duration(self):
         self.form_input['duration'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_reject_blank_topic(self):
         self.form_input['topic'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_accept_blank_teacher(self):
         self.form_input['teacher'] = ''
-        form = LessonRequestForm(data = self.form_input)
+        form = EditForm(data = self.form_input)
         self.assertTrue(form.is_valid()) 
