796091ef4cf7f79f46c3669a5f2247289dc8ca6b
Baldeep <baldeep.gill@kcl.ac.uk>
2022-11-24 16:45:47 +0000

    Fixed lesson request view tests

--- lessons/tests/views/test_lesson_request_view.py
+++ lessons/tests/views/test_lesson_request_view.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import login
 from lessons.forms import LessonRequestForm
 from lessons.models import Student, LessonRequest
 
@@ -22,10 +23,10 @@ class LessonRequestViewTestCase(TestCase):
         }
 
     def test_request_url(self):
-        self.assertEqual(self.url, '/lesson_request/')
+        self.assertEqual(self.url, '/student/lesson_request/')
 
-    def test_get_request_page(self):
-        self.client.login(username=self.student.username, password="Password123")
+    def test_get_request(self):
+        self.client.force_login(self.student)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "lesson_request.html")
@@ -35,8 +36,14 @@ class LessonRequestViewTestCase(TestCase):
         messages_list = list(response.context['messages'])
         self.assertEqual(len(messages_list), 0)
 
+    def test_not_logged_in_get_request(self):
+        redirect_url = reverse("log_in") + f"?next={self.url}"
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
     def test_unsuccessful_post(self):
         self.form_input['interval'] = -1
+        self.client.force_login(self.student)
         response = self.client.post(self.url, self.form_input)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lesson_request.html')
