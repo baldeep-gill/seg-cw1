9ddad50fb78b8ce60b484f002dce61b201bdb5dc
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-26 17:11:15 +0000

    refactor urls by removing duplicate ones and reflecting changes in .html files

--- msms/urls.py
+++ msms/urls.py
@@ -24,23 +24,18 @@ urlpatterns = [
     path('student_sign_up/', views.student_sign_up, name='student_sign_up'),
     path('log_in/', views.log_in, name='log_in'),
 
-    path('student/home/', views.student_home, name='student_home'),
-    path('student/lesson_request/', views.lesson_request, name='lesson_request'),
-    path('student/log_out/', views.log_out, name='log_out'),
-    path('admin/log_out/', views.log_out, name='log_out'),
-
-
+    # Admin paths
     path('admin/home/', views.admin_home, name='admin_home'),
-    path('admin/unfulfilled_requests/', views.admin_requests, name='admin_requests'),
+    path('admin/unfulfilled/requests/', views.admin_requests, name='admin_requests'),
     path('admin/book_lesson_request/<int:request_id>', views.book_lesson_request, name='book_lesson_request'),
-
-
-    path('admin/requests', views.admin_requests, name='admin_requests'),
+    path('admin/log_out/', views.log_out, name='log_out'),
     
-    # for branch 06
-    path('student/lessons/list', views.lessons_success, name='lesson_list'),
-
+    # Student paths
+    path('student/lesson_request/', views.lesson_request, name='lesson_request'),
+    path('student/home/', views.student_home, name='student_home'),
     path('student/requests/', views.show_requests, name='show_requests'),
     path('student/requests/edit/<lesson_id>', views.edit_requests, name='edit_requests'),
     path('student/requests/delete/<lesson_id>', views.delete_requests, name='delete_requests'),
+    path('student/lessons/list', views.lessons_success, name='lesson_list'),
+    path('student/log_out/', views.log_out, name='log_out'),
 ]
