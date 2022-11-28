57e8ef2168e31142751ac376b2fa6e06b6cda3bf
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-11-28 15:01:19 +0000

    Add basic list invoices page
    - Added a page which lets user see their invoices
    - Very bare bones, will flesh it out in following commits

--- lessons/views.py
+++ lessons/views.py
@@ -200,3 +200,9 @@ def delete_requests(request, lesson_id):
     else:
         current_lesson.delete()
         return redirect('show_requests')
+
+
+def show_invoices(request):
+    current_student = request.user
+    invoices = Invoice.objects.filter(student=current_student)
+    return render(request, 'invoices_list.html', {'invoices': invoices})
