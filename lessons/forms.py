3d05693f57616c7fe949be995eb4cd9ef66bf268
Serge <syraj.alkhalil@kcl.ac.uk>
2022-11-28 20:32:08 +0000

    add a guardian signup form

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.core.validators import RegexValidator, MinValueValidator
-from .models import User, Student, StudentProfile, LessonRequest, Lesson
+from .models import User, Student, StudentProfile, LessonRequest, Lesson, Guardian
 from django.db.models import Max
 from .helpers import find_next_available_student_number, day_of_the_week_validator
 from django.contrib.admin.widgets import AdminDateWidget
 from django.forms.fields import DateTimeField
 from django.core.exceptions import ValidationError
+import random
 
 class LessonRequestForm(forms.ModelForm):
     """Form for requesting a lesson"""
@@ -49,7 +50,6 @@ class StudentSignUpForm(forms.ModelForm):
 
         model = Student
         fields = ['first_name', 'last_name', 'email']
-        widgets = { 'bio': forms.Textarea() }
 
     new_password = forms.CharField(
         label='Password',
@@ -85,6 +85,50 @@ class StudentSignUpForm(forms.ModelForm):
         )
 
         StudentProfile.objects.create(user=student, student_number = new_student_number)
+        return student
+
+class GuardianSignUpForm(forms.ModelForm):
+    """Form enabling unregistered students to sign up."""
+
+    class Meta:
+        """Form options."""
+
+        model = Guardian
+        fields = ['first_name', 'last_name', 'email']
+
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+            )]
+    )
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        """Clean the data and generate messages for any errors."""
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    def save(self):
+        """Create a new user."""
+        random_number = random.randint(0, 1000000)
+
+        super().save(commit=False)
+        student = Student.objects.create_user(
+            username = self.cleaned_data.get('first_name') + self.cleaned_data.get('last_name') + f'{random_number}' ,
+            first_name=self.cleaned_data.get('first_name'),
+            last_name=self.cleaned_data.get('last_name'),
+            email=self.cleaned_data.get('email'),
+            password=self.cleaned_data.get('new_password'),
+        )
+
+        StudentProfile.objects.create(user=student, student_number = random_number)
         return student
 
 class PasswordForm(forms.Form):
