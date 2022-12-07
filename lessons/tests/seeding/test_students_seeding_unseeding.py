e09402473d78e32afecbcb72431e948894261436
Serge <syraj.alkhalil@kcl.ac.uk>
2022-12-07 21:07:57 +0000

    uncomment tests for seeding

--- lessons/tests/seeding/test_students_seeding_unseeding.py
+++ lessons/tests/seeding/test_students_seeding_unseeding.py
@@ -1,33 +1,33 @@
-# # TODO Uncomment this when want to test seeder
+# TODO Uncomment this when want to test seeder
 
-# from django.test import TestCase
-# from lessons.models import Student, Admin, User, StudentProfile
-# from django.core.management import call_command
+from django.test import TestCase
+from lessons.models import Student, Admin, User, StudentProfile
+from django.core.management import call_command
 
-# # Create your tests here.
+# Create your tests here.
 
-# class UserSeedTestCase(TestCase):
+class UserSeedTestCase(TestCase):
 
-#     def test_seeding_creates_expected_number_of_users(self):
-#         call_command('seed')
-#         # the seeder creates 100 student users using Faker
-#         # as well as 1 user with specified information (John Doe)
-#         student_count = Student.students.count()
-#         admins_count = Admin.admins.count()
-#         user_count = User.objects.count()
-#         student_profile_count = StudentProfile.objects.count()
-#         self.assertEqual(student_count, 101)
-#         self.assertEqual(admins_count, 2)
-#         self.assertEqual(student_profile_count, 101)
-#         self.assertEqual(user_count, 103)
+    def test_seeding_creates_expected_number_of_users(self):
+        call_command('seed')
+        # the seeder creates 100 student users using Faker
+        # as well as 1 user with specified information (John Doe)
+        student_count = Student.students.count()
+        admins_count = Admin.admins.count()
+        user_count = User.objects.count()
+        student_profile_count = StudentProfile.objects.count()
+        self.assertEqual(student_count, 101)
+        self.assertEqual(admins_count, 2)
+        self.assertEqual(student_profile_count, 101)
+        self.assertEqual(user_count, 103)
 
-#     def test_unseeding_clears_student_users(self):
-#         call_command('unseed')
-#         student_count = Student.students.count()
-#         admins_count = Admin.admins.count()
-#         student_profile_count = StudentProfile.objects.count()
-#         user_count = User.objects.count()
-#         self.assertEqual(student_count, 0)
-#         self.assertEqual(admins_count, 0)
-#         self.assertEqual(student_profile_count, 0)
-#         self.assertEqual(user_count, 0)
+    def test_unseeding_clears_student_users(self):
+        call_command('unseed')
+        student_count = Student.students.count()
+        admins_count = Admin.admins.count()
+        student_profile_count = StudentProfile.objects.count()
+        user_count = User.objects.count()
+        self.assertEqual(student_count, 0)
+        self.assertEqual(admins_count, 0)
+        self.assertEqual(student_profile_count, 0)
+        self.assertEqual(user_count, 0)
