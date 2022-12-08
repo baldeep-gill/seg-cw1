3516f7b0bbc27efce7bbd21c5f28ed87e0292b15
muhsin <muhsin.ma.mohamed@gmail.com>
2022-12-08 09:56:27 +0000

    Create seeder unseeder functions for new models
    - Created seeding for:
    - Lesson bookings
    - Fulfilled Lesson
    - Paid, Unpaid, Partially paid and overpaid transfers

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -2,32 +2,48 @@ import pytz
 from django.core.management.base import BaseCommand, CommandError
 from django.db.utils import IntegrityError
 from django.utils import timezone
+from django.urls import reverse
 from faker import Faker
-from lessons.models import Student, StudentProfile, Admin, LessonRequest, Term
-from lessons.helpers import find_next_available_student_number
+from lessons.models import Student, StudentProfile, Admin, LessonRequest, Term, Guardian, Invoice, Lesson, GuardianProfile, User
+from lessons.helpers import find_next_available_student_number, find_next_available_transfer_id, find_next_available_invoice_number_for_student, get_next_given_day_of_week_after_date_given
 from datetime import datetime, timedelta
 from django.core.management import call_command
 import pytz
+from django.test import TestCase
+from itertools import chain
 
-class Command(BaseCommand):
+
+class Command(BaseCommand, TestCase):
     PASSWORD = "Password123"
     USER_COUNT = 101
     # Includes 100 students, 1 additional admin
-    LESSON_COUNT = 15
+    LESSON_REQUEST_COUNT = 30
     # TODO: Use the LESSON_COUNT and USER_COUNT in tests rather than manually written numbers
+        
+    PROBABILITY_OF_FULFILLED_REQUESTS = 0.8
+
+    FULLY_PAID_PROBABILITY = 0.7
+    UNDERPAID_PROBABILITY = 0.3
+    OVERPAID_PROBABILITY = 0.2
+    UNPAID_PROBABLITY = 0.5
     
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
 
+
     def handle(self, *args, **options):
         call_command('unseed')
+
         self.seedusers()
         self.seedterms()
-        self.seedlessonrequests()
+
+        self.seedunfilfilledlessonrequests()
+        self.seedfulfilledrequests()
+
     
     def seedusers(self):
-        self._create_named_student_user(firstname="John", lastname="Doe", email="john.doe@example.com", password="Password123", uname=self._username("John", "Doe"))
+        self._create_named_client_user(firstname="John", lastname="Doe", email="john.doe@example.org", password="Password123", uname=self._username("John", "Doe"))
         self._create_named_admin_user(firstname="Petra", lastname="Pickles", email="petra.pickles@example.org", password="Password123", uname=self._username("Petra", "Pickles"))
 
         user_count = 0        
@@ -41,8 +57,10 @@ class Command(BaseCommand):
                 continue
             user_count += 1
         print('User seeding complete')
-        print('Seeded ' + str(Student.objects.count()) + ' students')
-        print('Seeded ' + str(Admin.objects.count()) + ' admins')
+        print('Seeded ' + str(User.objects.filter(type="STUDENT").count()) + ' students')
+        print('Seeded ' + str(User.objects.filter(type="ADMIN").count()) + ' admins')
+        print('Seeded ' + str(User.objects.filter(type="GUARDIAN").count()) + ' guardians')
+
 
     def seedterms(self):
         self._create_term('Term one',timezone.datetime(year=2022,month=9,day=1,tzinfo=pytz.UTC),timezone.datetime(year=2022,month=10,day=21,tzinfo=pytz.UTC))
@@ -54,9 +72,9 @@ class Command(BaseCommand):
         print('term seeding complete')
         print('Seeded ' + str(Term.objects.count()) + ' terms')
         
-    def seedlessonrequests(self):
+    def seedunfilfilledlessonrequests(self):
         lesson_count = 0        
-        while lesson_count < Command.LESSON_COUNT:
+        while lesson_count < Command.LESSON_REQUEST_COUNT:
             print(f'Seeding user {lesson_count}',  end='\r')
             try:
                 # seeds 1 admin after 100 students
@@ -67,13 +85,111 @@ class Command(BaseCommand):
         print('lesson request seeding complete')
         print('Seeded ' + str(LessonRequest.objects.count()) + ' requests')
 
+    def book_request(self, student, start_date, interval_between_lessons, number_of_lessons, duration, topic, teacher, day, time):
+            #combines the start date picked and the time each day into one dateTime object
+            new_date = datetime(start_date.year,start_date.month,start_date.day,time.hour,time.minute,tzinfo=pytz.UTC)
+            new_date = get_next_given_day_of_week_after_date_given(new_date,day)
+
+            #generate an invoice for the lessons we will generate
+            new_invoice_number = find_next_available_invoice_number_for_student(student)
+            invoice = Invoice.objects.create(
+                student=student,
+                date=datetime.now(tz=pytz.UTC),
+                invoice_number=new_invoice_number,
+            )
+            invoice.save()
+
+            #we will generate a lesson every lesson interval weeks at the time given
+            tdelta = timedelta(weeks=interval_between_lessons)
+            for i in range(number_of_lessons):
+                lesson = Lesson.objects.create(
+                    student=student,
+                    invoice=invoice,
+                    date=new_date,
+                    duration=duration,
+                    topic=topic,
+                    teacher=teacher
+                )
+                lesson.save()
+                new_date = new_date + tdelta
+            
+            return Lesson.objects.filter(invoice=invoice)
+    
+    def seedfulfilledrequests(self):
+        request_count = 0
+        for student in list(chain(Student.objects.all(), Guardian.objects.all())):
+            print(f'Seeded {request_count} lessons and invoices',  end='\r')
+            if self.faker.random.random() < Command.PROBABILITY_OF_FULFILLED_REQUESTS:
+                generated_lessons = self._create_booked_lesson(student)
+                request_count += generated_lessons.count()
+                for lesson in generated_lessons:
+                    random_val = self.faker.random.random()
+                    amount = lesson.invoice.price
+                    if random_val < Command.OVERPAID_PROBABILITY:
+                        amount = random_val*100 + lesson.invoice.price
+                    elif random_val < Command.UNDERPAID_PROBABILITY:
+                        # Ensures that the value doesn't go below zero
+                        amount = max(lesson.invoice.price - random_val*100, random_val*100)
+                    elif random_val < Command.UNPAID_PROBABLITY:
+                        # Doesn't generate the invoice
+                        continue
+                    elif random_val < Command.FULLY_PAID_PROBABILITY:
+                        # Value remains the invoice price
+                        pass
+                    self.generate_transfer_for_lesson(amount=amount, lesson=lesson)
+        print(f'For {Student.objects.count()} students, generated {Lesson.objects.count()} lessons and {Invoice.objects.count()} invoices')
+    
+    def _create_booked_lesson(self, client=None):
+        if client == None:
+            # Random student
+            client = Student.objects.order_by('?').first()
+        lessonRequest = self._create_lesson()
+        # select random term
+        term = Term.objects.order_by('?').first()
+        # Random day name
+        day = self.faker.date_time().strftime("%A")
+
+        lessonNum = self.faker.random.randint(1, 5)
+        interval_between_lessons = self.faker.random.randint(1, lessonNum)
+
+        return self.book_request(
+            student=client,
+            start_date=term.start_date,
+            interval_between_lessons=interval_between_lessons,
+            number_of_lessons=lessonNum,
+            duration=round(self.faker.random.randint(30, 120)),
+            time=self.faker.time_object(),
+            topic=self.faker.sentence(nb_words=2),
+            teacher=self.faker.name(),
+            day=day)
+
+        # self.client.post(lessonbookingurl, book_request_form_input)
+
+        
+
+        # transferurl = reverse('approve_transaction', kwargs={'student_id':lessonRequest.student.id, 'invoice_id':invoice.invoice_number})
+        # self.client.post(transferurl, confirm_transfer_form_input)
+
+    def generate_transfer_for_lesson(self, amount, lesson):
+        invoice = lesson.invoice
+
+        next_transfer_id = find_next_available_transfer_id()
+
+        confirm_transfer_form_input = {
+            "date_received": timezone.now(),
+            "transfer_id": next_transfer_id,
+            "amount_received": amount,
+            "verifier": Admin.objects.get(email="petra.pickles@example.org"),
+            "invoice": invoice
+        }
+
     def _create_user(self, isAdmin=False):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
         username = self._username(first_name, last_name)
         if(isAdmin): self._create_named_admin_user(first_name, last_name, email, username, Command.PASSWORD)
-        else: self._create_named_student_user(first_name, last_name, email, username, Command.PASSWORD)
+        else: self._create_named_client_user(first_name, last_name, email, username, Command.PASSWORD)
 
     def _create_term(self,name,start_date,end_date):
         try:
@@ -91,7 +207,7 @@ class Command(BaseCommand):
         user = Student.objects.order_by('?').first()
         lessonNum = self.faker.random.randint(1, 5)
         interval = self.faker.random.randint(1, lessonNum)
-        duration = round(self.faker.random.randint(30, 180))
+        duration = round(self.faker.random.randint(30, 120))
 
 
         availability_start = self.faker.date_time_this_year(after_now=True, before_now=False)
@@ -111,18 +227,21 @@ class Command(BaseCommand):
                 teacher=teacher
             )
         lessonRequest.save()
+
+        return lessonRequest
         
     '''uname stands for username'''
-    def _create_named_student_user(self, firstname, lastname, email, uname, password):
+    def _create_named_client_user(self, firstname, lastname, email, uname, password):
         studentnumber = find_next_available_student_number()
-        created_student = Student.objects.create_user(
+        created_student = Guardian.objects.create_user(
             first_name=firstname,
             last_name=lastname,
             email=email,
             username=uname,
             password=password,
         )
-        StudentProfile.objects.create(user=created_student, student_number=studentnumber)
+        GuardianProfile.objects.create(user=created_student, student_first_name=firstname, student_last_name=lastname, student_email=email)
+        # GuardianProfile.objects.create(user=created_student, student_number=studentnumber)
 
     '''uname stands for username'''
     def _create_named_admin_user(self, firstname, lastname, email, uname, password):
